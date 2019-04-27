7b319209884ac9d82b53c6598ff5ed768a45335a
Wilson Lin <wilsonwinhi@gmail.com>
2019-04-27 16:42:42 -0400

    add main method

--- VirusSimulation/src/virusSimulation/AdjPreprocess.java
+++ VirusSimulation/src/virusSimulation/AdjPreprocess.java
@@ -1,6 +1,8 @@
 package virusSimulation;
 import java.util.*;
 public class AdjPreprocess {
+	private int totalPeople = 0;
+	Set<String> peopleSet = new HashSet<>();
     public Map<String, Set<String>> adjPreprocess(List<String> s) {
     	Map<String, Set<String>> adjList = new HashMap<>();
     	System.out.println(s.get(0));
@@ -10,7 +12,15 @@ public class AdjPreprocess {
 				adjList.put(people[0], new HashSet<>());
 			}
 			adjList.get(people[0]).add(people[1]);
+			peopleSet.add(people[0]);
+			peopleSet.add(people[1]);
 		}
+		totalPeople = peopleSet.size();
 		return adjList;
     }
+
+	public int getTotalPeople() {
+		// TODO Auto-generated method stub
+		return totalPeople;
+	}
 }
--- VirusSimulation/src/virusSimulation/Simulation.java
+++ VirusSimulation/src/virusSimulation/Simulation.java
@@ -4,10 +4,21 @@ import java.util.*;
 
 public class Simulation {
 	public static void main(String[] args) {
+		int argc = args.length;
 		FileReaderTxt myText = new FileReaderTxt("facebook_combined (1).txt");
 		List<String> textList = myText.getAllLines();
 		AdjPreprocess preprocessMachine = new AdjPreprocess();
+		int totalPeople = preprocessMachine.getTotalPeople();
+		/* preprocess to adjacency list */
 		Map<String, Set<String>> adjList = preprocessMachine.adjPreprocess(textList);
-	
+		/* virus probability */
+		double probability = Double.parseDouble(args[0]);
+		/* seed */
+		int seedNum = Integer.parseInt(args[1]);
+		/* create seed into queue */
+		Queue<String> q = new LinkedList<>();
+		
+		/* run bfs */
+		// BFS1Round
 	}
 }
