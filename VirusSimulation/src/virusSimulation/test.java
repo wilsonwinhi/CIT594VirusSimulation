09442475391e186b6fba6590b21bdc1d743de149
szhongyu <49887481+szhongyu@users.noreply.github.com>
2019-04-27 18:06:56 -0400

    Add files via upload

--- VirusSimulation/src/virusSimulation/test.java
+++ VirusSimulation/src/virusSimulation/test.java
@@ -6,23 +6,20 @@ import java.util.*;
 public class test {
 
 	
-	public int BFS1Round(Map<People,Set<People>> map, Queue<People> storage, double virusProb) {
+	public int BFS1Round(Queue<People> storage, double virusProb) {
 		int count = 0;
 		//Random random = new Random();
 		int size = storage.size();
 		for(int i = 0;i<size;i++) {
 			People current = storage.poll();
-			if(current instanceof HealthyPeople || 
-					current instanceof TreatedPeople) {
+			if(current.isHealthy==true || 
+					current.isTreated==true) {
 				continue;
 			}
 			//List<People> netWork = ((UnhealthyPeople)current).socialNetwork;
-			if(!map.containsKey(current)) {
-				continue;
-			}
-			Set<People> netWork = map.get(current); 
+			List<People> netWork = current.getSocialNetwork(); 
 			for(People each: netWork) {
-				if(each instanceof UnhealthyPeople || each instanceof TreatedPeople) {
+				if(each.isTreated==true || each.isHealthy==false) {
 					continue;
 				}
 				//double eachResistance = ((HealthyPeople)each).resistence;
@@ -30,9 +27,7 @@ public class test {
 				//int randomNumber = random.nextInt(101);
 				//if(randomNumber<overall*100) {
 					count++;
-					String eachName = ((HealthyPeople)each).getName();
-					List<People> eachNetwork = ((HealthyPeople)each).getSocialNetwork();
-					each = new UnhealthyPeople(eachName,0,eachNetwork);
+					each.isHealthy = false;
 					storage.offer(each);
 				//}
 				
