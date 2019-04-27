fda7843830b554317d905af6a648190f242806e7
Wilson Lin <wilsonwinhi@gmail.com>
2019-04-27 14:58:15 -0400

    add virus class

---
+++ VirusSimulation/src/virusSimulation/unmutableVirus.java
@@ -0,0 +1,35 @@
+package virusSimulation;
+
+public class unmutableVirus implements Virus {
+
+	@Override
+	public boolean setProbability(double d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getProbability() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setVirusName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double mutateToStrong() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double mutateToWeak() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
