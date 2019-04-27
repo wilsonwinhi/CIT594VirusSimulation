3ab83bc4a225a32aab89521261af22a26457cb08
Wilson Lin <wilsonwinhi@gmail.com>
2019-04-27 14:54:56 -0400

    add interface method

--- VirusSimulation/src/virusSimulation/Virus.java
+++ VirusSimulation/src/virusSimulation/Virus.java
@@ -2,4 +2,18 @@ package virusSimulation;
 
 public interface Virus {
 
+
+    
+    public boolean setProbability(double d);
+    
+    
+    public double getProbability();
+
+
+    public void setVirusName(String name);
+
+
+    public double mutateToStrong();
+    
+    public double mutateToWeak();
 }
