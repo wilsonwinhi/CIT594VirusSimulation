b0ea6251f5c40a7620b58c019b7f925bf7e37eeb
szhongyu <49887481+szhongyu@users.noreply.github.com>
2019-04-27 15:00:42 -0400

    Add files via upload

---
+++ VirusSimulation/src/virusSimulation/VirusSimulation.java
@@ -0,0 +1,24 @@
+package virusSimulation;
+
+import java.util.*;
+
+public class VirusSimulation {
+
+	double virusProb;
+	List<People> graph;
+	boolean treatment;
+	int testTreatMethod;
+	int numberOfDays;
+	double seed;
+	String x;
+	String y;
+	
+	public void run() {
+		
+	}
+	
+	public void treat(int treatMethod) {
+		
+	}	
+	
+}
