8a54a320391e36ec58bac4a6f7ff8412606f9667
szhongyu <49887481+szhongyu@users.noreply.github.com>
2019-04-28 22:52:38 -0400

    Add files via upload

--- VirusSimulation/src/virusSimulation/People.java
+++ VirusSimulation/src/virusSimulation/People.java
@@ -2,13 +2,17 @@ package virusSimulation;
 
 import java.util.*;
 
-public class People {
+public class People implements Node{
 	String name;
 	double resistence;
 	List<People> socialNetwork;
 	boolean isHealthy;
 	boolean isTreated;
 	
+	/**
+	 *  This is the constructor
+	 * @param name
+	 */
 	public People(String name) {
 		this.name = name;
 		isHealthy = true;
@@ -16,34 +20,65 @@ public class People {
 		socialNetwork = new ArrayList<>();
 	}
 	
+	
+	/**
+	 * This function aims to make the people unhealthy 
+	 */
 	public void gotInfected() {
 		//do nothing there, healthy people cann't infect other people
 		isHealthy = false;
 	}
 	
+	/**
+	 * This is the getter of name and override the method in Node interface
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * This is the getter of resistence 
+	 * @return the value of resistence
+	 */
 	public double getResistence() {
 		return this.resistence;
 	}
 	
+	/**
+	 * This is the getter of socialNetwork
+	 * @return the list of socialNetwork
+	 */
 	public List<People> getSocialNetwork(){
 		return this.socialNetwork;
 	}
 	
-	
+	/**
+	 * This is the setter of resistence
+	 * @param the value of resistence
+	 */
 	public void setResistence(double value) {
 		this.resistence = value;
 	}
 	
+	/**
+	 * This is the setter of socialNetwork
+	 * @param the list of socialNetwork
+	 */
 	public void setNetwork(List<People> network) {
 		this.socialNetwork = network;
 	}
+	
+	/**
+	 * This function aims add a people into a this person's socialNerwork
+	 * @param People p
+	 */
 	public void addPeople(People p) {
 		socialNetwork.add(p);
 	}
+	
+	/**
+	 * This function override equals method in the Node interface 
+	 */
 	@Override
     public boolean equals(Object o) { 
   
